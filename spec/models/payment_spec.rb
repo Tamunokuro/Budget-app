3b2a1cb14eccc0928367f263a6524fd66e97d8f8
Tamunokuro <tamunokrobluejack@gmail.com>
2023-04-07 11:05:54 +0100

    Add Integration and Unit test

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe Payment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'Payment', type: :model do
+  before do
+    user = User.new(name: 'Jamie', email: 'jamie@gmail.com', password: 'jamiepass')
+    user.skip_confirmation!
+    user.save!
+    group = Group.new(name: 'Group 1', icon: 'group.png', user_id: user.id)
+    group.save!
+    payment = Payment.new(name: 'Payment 1', amount: 100, user_id: user.id)
+    payment.save!
+  end
+
+  it 'should be valid' do
+    expect(Payment.first).to be_valid
+  end
+
+  it 'should be invalid' do
+    payment = Payment.new(name: 'Payment 1', amount: 100, user_id: 1)
+    expect(payment).to_not be_valid
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'User Registration', type: :feature do
+  scenario 'should fail' do
+    visit new_user_registration_path
+    fill_in 'Name', with: 'Jamie'
+    fill_in 'Email', with: ''
+    fill_in 'Password', with: 'jamiepass'
+    fill_in 'Password confirmation', with: 'jamiepass'
+    click_button 'Sign up'
+    expect(page).to have_content('Email can\'t be blank')
+  end
+
+  scenario 'should fail' do
+    visit new_user_registration_path
+    fill_in 'Name', with: ''
+    fill_in 'Email', with: 'jamie@gmail.com'
+    click_button 'Sign up'
+    expect(page).to have_content('Name can\'t be blank')
+  end
 end
